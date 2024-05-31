cb2d7637d1fb43c01ca76fd0b51b56b85c3cde76
Sanket <sanketmahajan2003@gmail.com>
2024-05-31 13:21:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -7,31 +7,53 @@ import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <header className="App-header">
+          <h1>Welcome to My Portfolio</h1>
+          <nav>
+            <ul>
+              <li>
+                <button onClick={() => scrollToSection("home")}>Home</button>
+              </li>
+              <li>
+                <button onClick={() => scrollToSection("about")}>About</button>
+              </li>
+              <li>
+                <button onClick={() => scrollToSection("projects")}>
+                  Projects
+                </button>
+              </li>
+              <li>
+                <button onClick={() => scrollToSection("contact")}>
+                  Contact
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section id="home">
+            <Home />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
+          <section id="contact">
+            <Contact />
+          </section>
+        </main>
       </div>
     </Router>
   );
